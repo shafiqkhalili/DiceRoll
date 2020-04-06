fcd8ea52610e73e90e81f4f7f3a3abe35fb5b4bc
Shafigh Khalili <shafiqkhalili@gmail.com>
2020-04-06 14:57:51 +0200

    User XML instead of PNG

--- app/src/main/java/com/shafigh/dicegame/MainActivity.kt
+++ app/src/main/java/com/shafigh/dicegame/MainActivity.kt
@@ -1,51 +1,55 @@
 package com.shafigh.dicegame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceValue : TextView
-    lateinit var diceImage : ImageView
+    lateinit var diceValue: TextView
+    lateinit var diceImage: ImageView
+    lateinit var rollButton: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonRoll = findViewById<Button>(R.id.buttonRoll)
+        rollButton = findViewById(R.id.roll_button)
+        rollButton.text = "Lets Rolll"
 
-        diceValue = findViewById(R.id.textViewDiceValue)
-
-        diceImage = findViewById(R.id.imageDice)
-
-        buttonRoll.setOnClickListener{ v ->
+        rollButton.setOnClickListener {
+            //Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show()
             rollDice()
         }
 
     }
-    private fun rollDice(){
 
-        val randomNr = Random.nextInt(1,6)
+    private fun rollDice() {
+
+        diceValue = findViewById(R.id.result_text)
+
+        val randomNr = Random.nextInt(1, 6)
+
         diceValue.text = randomNr.toString()
 
+        diceImage = findViewById(R.id.dice_image)
 
 
-        var drawableresource = when(randomNr){
-            1 -> R.drawable.dice1
-            2 -> R.drawable.dice2
-            3 -> R.drawable.dice3
-            4 -> R.drawable.dice4
-            5 -> R.drawable.dice5
-            else -> R.drawable.dice6
+        var drawableresource = when (randomNr) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
         }
-        imageDice?.setImageResource(drawableresource)
+        diceImage?.setImageResource(drawableresource)
     }
 }
